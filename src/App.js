1a4fbabbd7fb137028a92c4a94c25b7332ed2b41
Brianna Bias <brianna.bias@gmail.com>
2021-06-23 00:38:10 -0700

    this is front end

--- src/App.js
+++ src/App.js
@@ -13,8 +13,31 @@ import DetailPage from './DetailPage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <h2>The Worst Page in the History of Pages</h2>
+        <p>
+          <Link to="/">
+            Home
+          </Link>
+        </p>
+        <p>
+          <Link to="/create">
+            Add a Character
+          </Link>
+        </p>
+        <Switch>
+          <Route path="/characters/:id" exact render={(routerProps) => <DetailPage {...routerProps} />}
+          />
+          <Route
+            path="/creat"
+            exact render={(routerProps) =>
+            <CreatePage {...routerProps} />
+            }
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
