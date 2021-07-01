56731520059ee16f48b8f61691984dcb4d0e0296
Brianna Bias <brianna.bias@gmail.com>
2021-07-01 08:49:35 -0700

    comment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ListPage from './ListPage';
 import CreatePage from './CreatePage';
 import DetailPage from './DetailPage';
 
+// this is a comment 
+
 export default class App extends Component {
   render() {
   return(
